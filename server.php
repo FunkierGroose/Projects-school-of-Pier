5ec201d0972243df44692a10f7b61628d38f71c7
FunkierGroose <51369666+FunkierGroose@users.noreply.github.com>
2020-04-24 23:14:12 +0200

    essential laravel
    Primo progetto usando il framework laravel sistema di messaggistica

--- server.php
+++ server.php
@@ -1,39 +1,21 @@
 <?php
 
-#new $_SESSION['UTENTE]
-$host = "127.0.0.1";
-$port = 12296;
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
 
-set_time_limit(0); //esecuzione all'infinito
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
 
-$sock = socket_create(AF_INET, SOCK_STREAM, 0) or die("Server: Non posso creare il socket!\n"); //Creazione socket
-$result = socket_bind($sock, $host, $port) or die ("Server: Non posso legare il socket al indirizzo\n"); //creazione legame socket con l'indirizzo del url su cui agisce
-
-$result = socket_listen($sock, 3) or die("Server: Non posso settare il server sull'ascolto del socket\n");//Creare il canale d'ascolto e mettere in attessa il server da eventuali risp
-
-class chat{
-
-    function readline(){
-
-        return rtrim(fgets(STDIN)); //formatto la stringa da spazi vuoti verso destra
-    }
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
 }
 
-do{
-    $accept = socket_accept($sock) or die("Server: Non posso accettare la richiesta di connessione del socket"); //apre la connessione con il client
-    $msg = socket_read($accept, 1024)or die("Server: Non posso leggere l'input dato\n");  //legge il pacchetto inviato dal client
-     
-    $msg = trim($msg); //eilimina gli spazi bianchi
-    print"Client:\t".$msg."\n\n"; //stampo il mex del client
-
-    $line = new chat(); //richiamo l'oggetto chat per una nuova risposta
-    print"Server risposta:\t";
-    $reply = $line->readline(); //input server
-
-    socket_write($accept, $reply, strlen($reply)) or die("Server: Non posso scrivere l'output\n"); //scrivo l'output
-
-}while(true); //esegui tutto cio fino a quando la connessione rimane aperta
-
-socket_close($accept, $sock); //chiudo
-
-?>
+require_once __DIR__.'/public/index.php';
